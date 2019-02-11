bede64ddeaad80c40b9ee7fd0c6b403315ee0bf3
Dan Bock <danbock@gmail.com>
2019-02-11 16:40:48 -0500

    Add login flow

---
+++ Loading.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import firebase from 'react-native-firebase'
+import { NavigationScreenProp } from 'react-navigation';
+
+interface LoadingProps {
+  navigation: NavigationScreenProp<any, any>
+}
+
+export default class Loading extends React.Component<LoadingProps> {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.props.navigation.navigate(user ? 'Main' : 'SignUp')
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Loading</Text>
+        <ActivityIndicator size="large" />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
---
+++ Main.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import firebase, { RNFirebase } from 'react-native-firebase';
+
+interface MainProps {
+
+}
+
+interface MainState {
+  currentUser: RNFirebase.User|null
+}
+
+export default class Main extends React.Component<MainProps, MainState> {
+  state: MainState = { currentUser: null }
+  
+  componentDidMount() {
+    const currentUser = firebase.auth().currentUser;
+    this.setState({ currentUser })
+  }
+
+  render() {
+    const { currentUser } = this.state
+    return (
+      <View style={styles.container}>
+        <Text>
+          Hi {currentUser && currentUser.email}!
+        </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
